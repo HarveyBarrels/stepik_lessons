5a3266ecc27d1b92cd108bcdaf53a8d6f0c58049
HarveyBarrels <evil0013@yandex.ru>
2020-12-08 21:00:35 +0300

    Модуль 5. преобразование теста в POM

---
+++ module_5/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en-gb",
+                     help="Choose language: ru, en-GB, es or fr")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    my_language = request.config.getoption("language")
+    my_options = Options()
+    my_options.add_argument(f"--lang={my_language}")
+
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(options=my_options)
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
